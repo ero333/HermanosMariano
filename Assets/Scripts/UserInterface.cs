4c2ddc2a235d5a702a18e574311b50509138192c
juan97flores <juan97flores@gmail.com>
2019-08-28 19:51:47 -0300

    *Programacion de la vida
    Co-Authored-By: desi27 <desi27@users.noreply.github.com>

--- Assets/Scripts/UserInterface.cs
+++ Assets/Scripts/UserInterface.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class UserInterface : MonoBehaviour
 {
     GameManager gm;
     public Text LiveText;
-    public Image[] EnergyBar;
 
-    // Start is called before the first frame update
+
+    public int Energy;
+    public int AllEnergy;
+    public Image[] EnergyBar;
+    public Sprite EnergyComplete;
+    public Sprite EnergyEmpty;
+    
     void Start()
     {
         gm = GetComponent<GameManager>();
@@ -19,7 +24,29 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Energy > AllEnergy)
+        {
+            Energy = AllEnergy;
+        } 
+
+        for (int i = 0; i < EnergyBar.Length; i++)
+        {
+            if (i < Energy)
+            {
+                EnergyBar[i].sprite = EnergyComplete;
+            }
+            else{
+                EnergyBar[i].sprite = EnergyEmpty;
+            }
+
+
+            if (i < AllEnergy) {
+                EnergyBar[i].enabled = true;
+            }
+            else {
+                EnergyBar[i].enabled = false;
+            }
+        }
     }
 
     
