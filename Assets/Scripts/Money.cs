938347f36ebe58cef7187cc2c650085b3c94d5bb
juan97flores <juan97flores@gmail.com>
2019-08-29 18:29:51 -0300

    +Programacion de billete

---
+++ Assets/Scripts/Money.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Money : MonoBehaviour
+{
+    public int value;
+    GameManager gm;
+
+    private void Start()
+    {
+        gm = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            gm.GainMoney(value);
+            Destroy(gameObject);
+        }
+    }
+   
+}
