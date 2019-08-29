9efbb42b402d832641c3134026301e9c5249ac47
JoaquinF30 <52161731+JoaquinF30@users.noreply.github.com>
2019-08-28 21:58:44 -0300

    +animacion y programacion de checkpoint

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -6,15 +6,20 @@ public class Checkpoint : MonoBehaviour
 {
     GameManager gm;
     bool active = false;
+    public Animator anim;
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player" && !active)
             gm.lastCheckpos = transform.position;
         active = true;
+        anim.SetTrigger("CheckpointColl");
+
     }
     void Start()
     {
         gm = GameObject.FindObjectOfType<GameManager>();
+        anim = GetComponent<Animator>();
     }
 }
