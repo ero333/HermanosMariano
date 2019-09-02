fb15d0125f7dc8a15ddae2a2a240c10eccd7a641
JoaquinF30 <52161731+JoaquinF30@users.noreply.github.com>
2019-09-02 19:32:25 -0300

    *Programacion y diseño de MenuPausa

---
+++ Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    public static bool juegoPausado = false;
+    public GameObject MenuPausaUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (juegoPausado)
+            {
+                Continuar();
+            }
+            else
+            {
+                Pausar();
+            }
+        }
+    }
+
+    public void Continuar()
+    {
+        MenuPausaUI.SetActive(false);
+        Time.timeScale = 1f;
+        juegoPausado = false;
+    }
+    void Pausar()
+    {
+        MenuPausaUI.SetActive(true);
+        Time.timeScale = 0f;
+        juegoPausado = true;
+    }
+    public void MenuPrincipal()
+    {
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+    public void VolverAlMapa()
+    {
+        SceneManager.LoadScene("VolverAlMapa");
+    }
+}   
+
+
