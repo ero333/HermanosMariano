73e3c6dd1884617b89310cad034cbee03cce6427
Ivanfs8 <buhosuarez@gmail.com>
2019-09-05 15:28:06 -0300

    *zona 1 del mapa *nivel en el mapa *que aparescan los niveles cuando pasa el mouse

---
+++ Assets/MapZoneButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapZoneButton : MonoBehaviour
+{
+    public GameObject[] levelBotones;
+
+    private void Start()
+    {
+        foreach (var levelBoton in levelBotones)
+        {
+            levelBoton.SetActive(false);
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        foreach (var levelBoton in levelBotones)
+        {
+            levelBoton.SetActive(true);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        foreach (var levelBoton in levelBotones)
+        {
+            levelBoton.SetActive(false);
+        }
+    }
+}
