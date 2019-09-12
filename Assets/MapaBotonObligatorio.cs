facf527f432a8eec73ef55bd3ac2b11291325439
Ivanfs8 <buhosuarez@gmail.com>
2019-09-12 17:14:58 -0300

    +se puede desbloquear deudas *no carga la cinematica de intro en la oficina

---
+++ Assets/MapaBotonObligatorio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapaBotonObligatorio : MonoBehaviour
+{
+    GameManager gm;
+    public GameObject Deuda;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    public void Unlock (int deuda)
+    {
+        if(gm.ahorros >= deuda)
+        {
+            gm.ahorros -= deuda;
+            gameObject.GetComponent<Button>().interactable = true;
+            gm.paidDeudas += 1;
+            Deuda.SetActive(false);
+        }
+        else
+        {
+            Deuda.GetComponent<Button>().interactable = false;
+        }
+    }
+}
--- Assets/Scripts/MapZone.cs
+++ Assets/Scripts/MapZone.cs
@@ -8,6 +8,8 @@ public class MapZone : MonoBehaviour
 {
     GameManager gm;
     public GameObject[] Zonas;
+    public GameObject[] Deudas;
+
     public Text ahorrostexto;
 
     // Start is called before the first frame update
@@ -15,14 +17,29 @@ public class MapZone : MonoBehaviour
     {
         gm = GameObject.FindObjectOfType<GameManager>();
 
+        gm.SaveData();
+
         for (int i = 0; i < Zonas.Length; i++)
         {
             Zonas[i].GetComponent<Selectable>().interactable = false;
+
+            if (i <= gm.zoneProgress)
+            {
+                Zonas[i].GetComponent<Selectable>().interactable = true;
+            }
         }
 
-        for (int i = 0; i < gm.zoneProgress; i++)
+        //for (int i = 0; i < gm.zoneProgress; i++)
+        //{
+
+        //}
+
+        for (int i = 0; i < Deudas.Length; i++)
         {
-            Zonas[i].GetComponent<Selectable>().interactable = true;
+            if (i < gm.paidDeudas)
+            {
+                Deudas[i].SetActive(false);
+            }
         }
     }
 
@@ -30,6 +47,7 @@ public class MapZone : MonoBehaviour
     {
         SceneManager.LoadScene(levelName);
     }
+
     private void Update()
     {
         ahorrostexto.text = "" + gm.ahorros;
