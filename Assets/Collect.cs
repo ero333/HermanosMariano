8f4f7ed15f9c42a09e7365363cf0a62bcd6abb5e
Ivanfs8 <buhosuarez@gmail.com>
2019-09-12 19:13:33 -0300

    +condicion de recolectar

---
+++ Assets/Collect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect : MonoBehaviour
+{
+    GameManager gm;
+    UserInterface ui;
+
+    public int toCollect;
+
+    public int gananciaMaxima = 100;
+    public bool Obligatorio = false;
+
+    private void Start()
+    {
+        gm = GameObject.FindObjectOfType<GameManager>();
+        ui = GameObject.FindObjectOfType<UserInterface>();
+
+        toCollect = FindObjectsOfType<Collectable>().Length;
+    }
+
+    void Update()
+    {
+        if (toCollect <= 0)
+        {
+            gm.VictoryCondition();
+            ui.Victory(gananciaMaxima);
+        }
+    }
+}
---
+++ Assets/Collectable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    Collect collect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            collect = FindObjectOfType<Collect>();
+
+            collect.toCollect -= 1;
+
+            Destroy(gameObject);
+        }
+    }
+}
