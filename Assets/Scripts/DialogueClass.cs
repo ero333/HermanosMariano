d5dfc82649d78acca71d7166fd82bed3f13e92da
Ivanfs8 <buhosuarez@gmail.com>
2019-09-15 20:13:27 -0300

    +Herramienta de dialogos tipo otome

---
+++ Assets/Scripts/DialogueClass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+
+public class DialogueClass
+{
+    public string Speaker;
+
+    public enum PosibleCharacters { CharA, CharB };
+
+    public PosibleCharacters character; 
+
+    [Multiline]
+    public string Dialogue;
+
+    public Sprite CharArt;
+
+
+}
---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    [Header("Start")]
+    public Sprite charAstart;
+    public Sprite charBstart;
+
+    public DialogueClass[] Dialogues;
+
+    [Header("Referencias")]
+    public Text characterText;
+    public Text dialogueText;
+    public Image charA;
+    public Image charB;
+
+    int index = 0;
+
+    void Start()
+    {
+        if (Dialogues.Length == 0)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            Time.timeScale = 0;
+            charA.sprite = charAstart;
+            charB.sprite = charBstart;
+        }
+    }
+
+   
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Space))
+        {
+            index += 1;
+        }
+
+        if (index < Dialogues.Length)
+        {
+            dialogueText.text = Dialogues[index].Dialogue;
+
+            if (Dialogues[index].character == DialogueClass.PosibleCharacters.CharA)
+            {
+                charA.sprite = Dialogues[index].CharArt;
+                charA.color = new Color(1, 1, 1, 1f);
+
+                charB.color = new Color(1,1,1,0.5f);
+            }
+            else
+            {
+                charB.sprite = Dialogues[index].CharArt;
+                charB.color = new Color(1, 1, 1, 1f);
+
+                charA.color = new Color(1, 1, 1, 0.5f);
+            }
+        }
+        else
+        {
+            Time.timeScale = 1;
+            gameObject.SetActive(false);
+        }
+
+
+    }
+}
