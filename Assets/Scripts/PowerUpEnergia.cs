89be77f37494a44b13f8dd372b68ef33b316e672
JoaquinF30 <52161731+JoaquinF30@users.noreply.github.com>
2019-09-23 16:43:22 -0300

    +Programacion power up

---
+++ Assets/Scripts/PowerUpEnergia.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpEnergia : MonoBehaviour
+{
+    private void Start()
+    {
+        gm = GameObject.FindObjectOfType<GameManager>();
+    }
+    GameManager gm;
+    public int CantidadDeEnergia;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            gm.energy += CantidadDeEnergia;
+            Destroy(gameObject);
+        }
+    }
+}
