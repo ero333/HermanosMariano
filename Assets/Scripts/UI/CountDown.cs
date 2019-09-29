ce00bc994b80dd6e49b163776743f960dcd08cd6
JoaquinF30 <52161731+JoaquinF30@users.noreply.github.com>
2019-09-29 19:38:18 -0300

    +contador de tiempo

---
+++ Assets/Scripts/UI/CountDown.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    GameManager gm;
+    UserInterface ui;
+
+    float currentTime = 0.0f;
+    public float startingTime;
+
+    public Text countdownText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+        gm = GameObject.FindObjectOfType<GameManager>();
+        ui = FindObjectOfType<UserInterface>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            Time.timeScale = 0f;
+            ui.GameOverScreen.SetActive(true);
+        }
+    }
+}
