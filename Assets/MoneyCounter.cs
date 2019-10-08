735c6841a9a7270f96559a9e9e50295775cc9aed
Ivanfs8 <buhosuarez@gmail.com>
2019-10-08 20:40:50 -0300

    *billetes CiudadOpcional1 +Herramienta que va contando automaticamente los billetes en el editor

---
+++ Assets/MoneyCounter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.EditorTools;
+
+[ExecuteInEditMode]
+public class MoneyCounter : MonoBehaviour
+{
+    public int MaximusMoney;
+    Money[] allMoney;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allMoney = FindObjectsOfType<Money>();
+        MaximusMoney = 0;
+        for (int i = 0; i < allMoney.Length; i++)
+        {
+            MaximusMoney += allMoney[i].value;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode)
+        {
+            enabled = false;
+        }
+        else
+        {
+            allMoney = FindObjectsOfType<Money>();
+            MaximusMoney = 0;
+            for (int i = 0; i < allMoney.Length; i++)
+            {
+                MaximusMoney += allMoney[i].value;
+            }
+        }        
+    }
+}
