527bb2fe84e7a9a4dcc1b58d8769db29fba114e8
Ivanfs8 <buhosuarez@gmail.com>
2019-10-17 19:54:07 -0300

    +audio manager *se puede mutear el audio

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    AudioSource audioM;
+    private void Awake()
+    {
+        audioM = GetComponent<AudioSource>();
+        if (GameManager.sound)
+        {
+            audioM.mute = false;
+        }
+        else
+        {
+            audioM.mute = true;
+        }
+
+        audioM.Play();
+    }
+
+    void Update()
+    {
+        if (GameManager.sound)
+        {
+            audioM.mute = false;
+        }
+        else
+        {
+            audioM.mute = true;
+        }
+    }
+}
